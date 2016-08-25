4f91d5ea62385ff6303ae9929a4760c071158d28
ANDREA <andrea.palomino.ruby@gmail.com>
2016-08-25 14:10:54 -0700

    new rep

---
+++ java.js
@@ -0,0 +1,5 @@
+var num1 = parseInt(prompt("Teclea un numero entero"));
+var num2 = parseInt(prompt("Teclea otro numero entero"));
+var num3 = parseInt(prompt("Teclea otro numero entero"));
+
+document.write ((num1 + num2 + num3)/3);
